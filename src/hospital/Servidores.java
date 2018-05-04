55487b7828454bd4f9a65af765da44d3d35b7a0f
Squierx <merenda.franco83@gmail.com>
2018-05-04 00:02:37 -0300

    Asignacion De cola implementado

--- src/hospital/Servidores.java
+++ src/hospital/Servidores.java
@@ -1,6 +1,7 @@
 package hospital;
 
 import java.util.LinkedList;
+import fel.Queue;
 
 public class Servidores {
 
@@ -24,6 +25,66 @@ public class Servidores {
         for(int i=0;i<cantEsp;i++){
             this.getMedicoEspecialista().add(new Servidor());
         }
+    }
+
+
+
+    public Queue asignacionCola(byte cuadroClinico){
+
+           Queue colaAsignada; //Variable con la cola a donde asignar el paciente.
+           int cantidadServidores; //Variable que contiene la cantidad de servidores por tipo de médico.
+           int colaCorta; //Variable que lleva la cantidad de la cola más corta.
+
+           switch (cuadroClinico){
+               //Cuadro Leve
+               case 0:{
+                   cantidadServidores= this.medicoResidente.size();
+                   colaCorta=this.medicoResidente.get(0).getCola().getCantidadItems();
+                   colaAsignada=this.medicoResidente.get(0).getCola();
+
+                   //Ya revise en i=0, por lo tanto arranco desde i=1
+                   for(int i=1;i<cantidadServidores;i++)
+                       if (colaCorta > this.medicoResidente.get(i).getCola().getCantidadItems()) {
+
+                            colaCorta = this.medicoResidente.get(i).getCola().getCantidadItems();
+                            colaAsignada = this.medicoResidente.get(i).getCola();
+                       }
+                       return colaAsignada;
+               }
+
+               //Cuadro Medio
+               case 1:{
+                   cantidadServidores= this.medicoGeneral.size();
+                   colaCorta=this.medicoGeneral.get(0).getCola().getCantidadItems();
+                   colaAsignada=this.medicoGeneral.get(0).getCola();
+
+                   //Ya revise en i=0, por lo tanto arranco desde i=1
+                   for(int i=1;i<cantidadServidores;i++)
+                       if (colaCorta > this.medicoGeneral.get(i).getCola().getCantidadItems()) {
+
+                           colaCorta = this.medicoGeneral.get(i).getCola().getCantidadItems();
+                           colaAsignada = this.medicoGeneral.get(i).getCola();
+                       }
+                   return colaAsignada;
+               }
+
+               //Cuadro Grave
+               case 2:{
+                   cantidadServidores= this.medicoEspecialista.size();
+                   colaCorta=this.medicoEspecialista.get(0).getCola().getCantidadItems();
+                   colaAsignada=this.medicoEspecialista.get(0).getCola();
+
+                   //Ya revise en i=0, por lo tanto arranco desde i=1
+                   for(int i=1;i<cantidadServidores;i++)
+                       if (colaCorta > this.medicoEspecialista.get(i).getCola().getCantidadItems()) {
+
+                           colaCorta = this.medicoEspecialista.get(i).getCola().getCantidadItems();
+                           colaAsignada = this.medicoEspecialista.get(i).getCola();
+                       }
+                   return colaAsignada;
+               }
+           }
+        return new Queue(); //Solo decorativo ya que no cumple ninguna funcion solo para que no muestre missing return statement
     }
 
     public LinkedList<Servidor> getMedicoResidente() {
